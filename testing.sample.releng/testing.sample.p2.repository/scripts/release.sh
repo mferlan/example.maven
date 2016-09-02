a4f2826e90e1541dab493292f42b9b579fe501a7
Martin Ferlan <martinferlan@atron.de>
2016-09-02 10:18:52 +0200

    scripts release

---
+++ testing.sample.releng/testing.sample.p2.repository/scripts/release.sh
@@ -0,0 +1,19 @@
+IF [%VERSION%] == [] SET /p VERSION=Enter release version
+IF [%NEWVERSION%] == [] SET /p NEWVERSION=Enter next development version
+
+# Change the version number to <version>
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.25.0:set-version -DnewVersion=$VERSION
+
+# make sure everything compiles
+mvn clean install
+
+# Commit and push the changes:
+git commit -a -m "Changed version number for $VERSION release ($VERSION)"
+git tag V_$VERSION
+git push origin
+git push origin V_$VERSION
+
+# Change the version number to <version+1>-SNAPSHOT and commit:
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.25.0:set-version -DnewVersion=$NEWVERSION-SNAPSHOT
+git commit -a -m "Changed version to $NEWVERSION-SNAPSHOT"
+git push
